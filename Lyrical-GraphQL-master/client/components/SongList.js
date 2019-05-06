c170879ae28676f89f77b7b61f0ce83be811c302
Tim Dommett <timdom11@gmail.com>
2019-05-06 15:49:52 +0200

    completesection 6

---
+++ Lyrical-GraphQL-master/client/components/SongList.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import gql from 'graphql-tag';
+import {graphql} from 'react-apollo';
+import { Link } from 'react-router';
+import query from '../queries/fetchSongs';
+// import delete from "../queries/deletSong";
+
+
+
+class SongList extends Component {
+
+    onSongDelete(id) {
+        // NB refreshing
+        this.props.mutate({variables: { id: id }})
+        .then(() => this.props.data.refetch());
+    }
+    
+    renderSongs() {
+        // at begining no data as graphql fetchs asynchronously so have to do case where undefined with every component that fetchs data from graphql
+
+        return this.props.data.songs.map(song => {
+            return (
+                <li key={song.id} className="collection-item">
+                    {song.title}
+                    <i className="material-icons" onClick={() => this.onSongDelete(song.id)}>delete</i>
+                    
+                </li>
+            );
+        });
+    }
+
+    render() {
+
+        if (this.props.data.loading) {
+            return <div>Loading...</div>;
+        }
+    return (
+                
+        <div>
+            <ul className="collection">
+                {this.renderSongs()}
+            </ul>
+            <Link to="/songs/new" className="btn-floating btn-large red right">
+            <i className="material-icons">add</i>
+            </Link>
+        </div>
+        
+        );
+    }
+}
+
+const mutation = gql`
+  mutation DeleteSong($id: ID) {
+    deleteSong(id: $id) {
+      id
+    }
+  }
+`;
+// const delete = delete;
+
+// defining Query (not excuting yet):
+// const query = gql`
+//     {
+//         songs {
+//             id
+//             title
+//         }
+//     }
+// `;
+
+export default graphql(mutation)(graphql(query)(SongList));
